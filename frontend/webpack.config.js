cd15303bb1e7bce9a0397b3db30d0837520228b2
Takács Vili <tvili999@gmail.com>
2022-04-02 12:05:20 +0200

    production webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,36 +1,39 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-    mode: "development",
-    devtool: "source-map",
-    resolve:{
-		alias: {
-			"react$": path.resolve(__dirname, "node_modules/react"),
-			"core": path.resolve(__dirname, 'src/core'), // eslint-disable-line
-     		"components": path.resolve(__dirname, 'src/components'), // eslint-disable-line
-     		"contexts": path.resolve(__dirname, 'src/contexts'), // eslint-disable-line
-		}
+module.exports = (env) => ({
+    mode: env.production ? "production" : "development",
+    devtool: env.production ? false : "source-map",
+    resolve: {
+        alias: {
+            react$: path.resolve(__dirname, "node_modules/react"),
+            core: path.resolve(__dirname, "src/core"), // eslint-disable-line
+            components: path.resolve(__dirname, "src/components"), // eslint-disable-line
+            contexts: path.resolve(__dirname, "src/contexts"), // eslint-disable-line
+        },
     },
     module: {
         rules: [
-            { test: /\.styl$/, use: [ "style-loader", "css-loader", "stylus-loader" ] },
-            { test: /\.js$/, use: [ "babel-loader" ] },
-        ]
+            {
+                test: /\.styl$/,
+                use: ["style-loader", "css-loader", "stylus-loader"],
+            },
+            { test: /\.js$/, use: ["babel-loader"] },
+        ],
     },
-	devServer: {
-		host: "0.0.0.0",
-		port: 8900,
-		proxy: {
-			'/api': {
-				target: 'http://backend:9000',
-				secure: false
-			}
-		}
-	},
+    devServer: {
+        host: "0.0.0.0",
+        port: 8900,
+        proxy: {
+            "/api": {
+                target: "http://backend:9000",
+                secure: false,
+            },
+        },
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ]
-}
+            template: "./src/index.html",
+        }),
+    ],
+});
